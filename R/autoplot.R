5665af8dd31d0e1784b04e9adf709346fe8c87e4
Tingting Zhan <tingtingzhan@gmail.com>
2025-02-28 22:47:41 -0500

    test

---
+++ R/autoplot.R
@@ -0,0 +1,73 @@
+
+
+#' @title autolayer.aggRate
+#' 
+#' @description ..
+#' 
+#' @param object ..
+#' 
+#' @param type ..
+#' 
+#' @param ... ..
+#' 
+#' @importFrom ggplot2 autolayer facet_grid label_both geom_line
+#' @importFrom reshape2 melt
+#' @export autolayer.aggRate
+#' @export
+autolayer.aggRate <- function(object, type = c('plain', 'cumulative', 'acat', 'cratio'), ...) {
+  
+  rnm <- deparse1(attr(object, which = 'formula', exact = TRUE)[[2L]])  
+  rc <- object[[rnm]] # 'cbind-ed ratings'
+  id.vars <- setdiff(names(object), rnm)
+  
+  dats <- lapply(list(
+    obs = rc/rowSums(rc),
+    yhat = attr(object, which = 'yhat', exact = TRUE),
+    #lower = object@.lower,
+    #upper = object@.upper,
+    cumulative = link_flatRate(object, type = 'cumulative'),
+    acat = link_flatRate(object, type = 'acat'),
+    cratio = link_flatRate(object, type = 'cratio')
+  ), FUN = function(i) {
+    if (!length(i)) return(invisible())
+    melt(data = data.frame(object[id.vars], i, check.names = FALSE), 
+         id.vars = id.vars, measure.vars = colnames(i), variable.name = rnm)
+  })
+  
+  type <- match.arg(type)
+  
+  mp <- eval(call(name = 'aes', x = as.symbol(rnm), y = quote(value), group = as.symbol(id.vars[1L]), colour = as.symbol(id.vars[1L])))
+  list(
+    switch(type, plain = list(
+      geom_line(data = dats$obs, mapping = mp),
+      if (length(dats$yhat)) geom_line(data = dats$yhat, mapping = mp, linetype = 2L)
+    ), geom_line(data = dats[[type]], mapping = mp)),
+    # not plotting the confidence interval yet
+    if (length(id.vars) > 1L) {
+      facet_grid(rows = call(name = '~', str2lang(paste(id.vars[-1L], collapse = '+')), quote(.)), 
+                 labeller = label_both)
+    }
+  )
+  
+}
+
+
+
+#' @importFrom ggplot2 autoplot ggplot scale_y_continuous labs
+#' @export
+autoplot.aggRate <- function(object, type = c('plain', 'cumulative', 'acat', 'cratio'), ...) {
+  type <- match.arg(type)
+  ggplot() + 
+    autolayer.aggRate(object, type = type, ...) +
+    switch(type, plain = {
+      scale_y_continuous(labels = function(x) sprintf(fmt = '%.0f%%', 1e2*x))
+    }, scale_y_continuous(trans = 'logit')) +
+    labs(y = switch(type, 
+                    plain = 'Conditional Probs.', 
+                    cumulative = 'Logit Conditional Cumulative Probs.', 
+                    acat = 'Log Adjacent-Categories-Ratio', 
+                    cratio = 'Logit Continuation-Ratio'))
+}
+
+
+
---
+++ R/flatRate.R
@@ -0,0 +1,292 @@
+#  #within.data.frame(expr = {
+#  # bitter = cbind(bitter1, bitter2, bitter3, bitter4, bitter5)
+#  # colnames(bitter) = NULL
+# # bitter1 = bitter2 = bitter3 = bitter4 = bitter5 = NULL
+#  #}) |>
+
+
+
+
+#setClass(Class = 'aggRate', contains = 'data.frame', slots = c(
+#  formula = 'formula',
+#  .est = 'matrix',
+#  .lower = 'matrix',
+#  .upper = 'matrix'
+#), prototype = prototype(
+#  .est = array(NA_real_, dim = c(0L, 0L)),
+#  .lower = array(NA_real_, dim = c(0L, 0L)),
+#  .upper = array(NA_real_, dim = c(0L, 0L))
+#))
+
+
+
+#' @title Create Aggregated 'Flat' Format of Rating Data
+#' 
+#' @description
+#' 
+#' Function [aggRate] creates aggregate counts rating data *flat* format,
+#' for \link[VGAM]{vglm} as well as human eyes.
+#' Each row represents the aggregated rating records for a single *product*, 
+#' and each column denotes the levels of rating (e.g. \link[VGAM]{wine}).
+#' 
+#' @param object ..
+#' 
+#' @param formula ..
+#' 
+#' @param data ..
+#' 
+#' @param pattern,rating ..
+#' 
+#' @param ... ..
+#' 
+#' @importFrom matrixStats rowCumsums
+#' 
+#' @examples 
+#' VGAM::wine |>
+#'  aggRate.data.frame(pattern = c(bitter = '^bitter'))
+#' 
+#' ordinal::wine |> 
+#'  aggRate.formula(formula = rating ~ temp + contact)
+#'  
+#' m = MASS::housing |>
+#'  reshape2::dcast(formula = Infl + Type + Cont ~ Sat, value.var = 'Freq') |>
+#'  aggRate.data.frame(rating = list(Sat = c('Low', 'Medium', 'High')))
+#' # printing method has a bug.
+#' 
+#' library(ordinal)
+#' clm(rating ~ temp * contact, data = wine) |>
+#'  aggRate.clm()
+#' # aggRate(housing_clm) # I plan to deprecate
+#' 
+#' library(VGAM)
+#' pneumo2 = VGAM::pneumo |>
+#'  aggRate.data.frame(rating = list(y = c('normal', 'mild', 'severe')))
+#' (m = vglm(y ~ log(exposure.time), family = multinomial, data = pneumo2))
+#' m |> aggRate.vlm()
+#' 
+#' @name aggRate
+#' @export
+aggRate <- function(object, ...) UseMethod(generic = 'aggRate')
+
+#' @rdname aggRate
+#' @export aggRate.data.frame
+#' @export
+aggRate.data.frame <- function(object, pattern, rating, ...) {
+  
+  # \pkg{VGAM}-type of data, see ?VGAM::wine
+  data <- object; object <- NULL
+  if (anyNA(data)) stop('do not allow missing in input')
+  
+  nm <- names(data)
+  if (!identical(nm, make.names(nm))) stop('input column names must be syntactically valid')
+  
+  if (!missing(pattern) || !missing(rating)) {
+    if (!missing(pattern)) {
+      new_nm <- names(pattern)
+      id <- grepl(pattern = pattern, x = nm)
+    } else if (!missing(rating)) {
+      new_nm <- names(rating)
+      id <- match(rating[[1L]], table = nm)
+    }
+    
+    if (!length(new_nm)) stop('`pattern` must be named')
+    if (!identical(make.names(new_nm), new_nm)) stop()
+    if (new_nm %in% nm) stop()
+    data[[new_nm]] <- as.matrix.data.frame(data[id])
+    data[nm[id]] <- NULL
+    nm <- names(data)
+  }
+  
+  id <- vapply(data, FUN = is.matrix, FUN.VALUE = NA)
+  if (sum(id) != 1L) stop('input must contain one and only one matrix-column')
+  
+  storage.mode(data[[nm[id]]]) <- 'integer' # need to check if they are really 'integer'
+  if (!length(colnames(data[[nm[id]]]))) {
+    colnames(data[[nm[id]]]) <- seq_len(dim(data[[nm[id]]])[2L])
+  }
+
+  class(data) <- c('aggRate', class(data))
+  attr(data, which = 'formula') <- eval(call(name = '~', as.symbol(nm[id]), str2lang(paste(nm[-id], collapse = '+'))), envir = .GlobalEnv)
+  return(data)
+
+}
+
+
+
+#' @rdname aggRate
+#' @importFrom stats aggregate
+#' @export aggRate.formula
+#' @export
+aggRate.formula <- function(formula, data, ...) {
+  
+  # \pkg{ordinal}-type of data, see ?ordinal::wine
+  
+  # check `formula` is two-sided; check `formula` is not recursive
+  #if (inherits(formula, what = 'terms')) class(formula) <- 'formula' # do I still need this??
+  y <- formula[[2L]]
+  if (!is.symbol(y)) stop('Response term must be \'symbol\'')
+  yval <- eval(y, envir = data)
+  if (is.matrix(yval)) .Defunct(new = 'aggRate.data.frame')
+  
+  if (!is.ordered(data[[y]])) data[[y]] <- ordered(data[[y]])
+
+  ret <- aggregate(formula, data = data, FUN = table) # wow!
+  # ?base::table return is typeof-integer
+  class(ret) <- c('aggRate', class(ret))
+  attr(ret, which = 'formula') <- formula
+  return(ret)
+  
+}
+
+
+
+
+#' @rdname aggRate
+#' @importFrom stats formula predict
+#' @export aggRate.clm
+#' @export
+aggRate.clm <- function(object, ...) {
+  call_ <- object$call
+  if (!is.data.frame(data <- eval(call_$data))) stop('`data` must be evaluable')
+  if (length(call_$weights)) stop('using of `weights` has been discontinued')
+  ret <- aggRate.formula(formula = formula(object), data = data)
+  attr(ret, which = 'yhat') <- cast_predict.clm(object)
+  return(ret)
+}
+
+
+cast <- reshape2:::cast
+array_names <- reshape2:::array_names
+cast_predict.clm <- function(object, ...) {
+  
+  fom <- formula(object)
+  cast_fom <- eval(call(name = '~', fom[[3L]], fom[[2L]]))
+  m <- unique.data.frame(object$model)
+  m$yhat <- predict(object, newdata = m, interval = FALSE)$fit
+    
+  pred_cast <- cast(data = m, formula = cast_fom, value.var = 'yhat', drop = FALSE)
+  
+  ret <- pred_cast$data
+  ret[is.na(ret)] <- 0
+  colnames(ret) <- array_names(pred_cast$labels[[2]]) # see ?reshape2::dcast
+  return(ret)
+  
+} 
+
+
+
+#' @rdname aggRate
+#' @importFrom VGAM formulavlm
+#' @export aggRate.vlm
+#' @export
+aggRate.vlm <- function(object, ...) {
+  if (!is.data.frame(data <- eval(object@call$data))) stop('`data` must be evaluable')
+  fom <- formulavlm(object) # ?VGAM::formula.vlm; ?VGAM::formulavlm
+  if (!is.symbol(fom[[2L]])) stop('Response term must be \'symbol\'')
+  # fit <- predict(object, se.fit = TRUE) # No!!
+  class(data) <- unique.default(c('aggRate', class(data)))
+  attr(data, which = 'formula') <- fom
+  attr(data, which = 'yhat') <- object@fitted.values # ?VGAM::fittedvlm
+  # `yhat`: old name `@.est`
+  return(data)
+}
+
+
+
+
+#' @importFrom stats nobs
+#' @export
+nobs.aggRate <- function(object, ...) sum(object[[deparse1(attr(object, which = 'formula', exact = TRUE)[[2L]])]])
+
+
+
+
+
+
+
+
+
+#' @export
+autoplot.vlm <- function(object, type = object@family@vfamily[1L], ...) {
+  cl <- match.call()
+  if (!length(cl$type) || cl$type != 'plain') {
+    cl$type <- 'plain'
+    message('Use ', deparse1(cl), ' to see the conditional probabilities')
+  }
+  autoplot.aggRate(aggRate.vlm(object), type = type, ...)
+} 
+
+
+
+
+
+#' @export
+print.aggRate <- function(x, ...) {
+  print(autoplot.aggRate(x, ...))
+}
+
+
+# 
+# \itemize{
+# 
+# \item{cumulative} {(no confidence ribbon) 
+# cumulative probabilities with x-variable design matrix}
+# 
+# \item{acat} {(no confidence ribbon)
+# probability ratios of adjacent categories,
+# `ln(P[Y=j+1]/P[Y=j])`} (without `ln`, 
+# which is accomplished by axis transformation in \link[ggplot2]{ggplot}) 
+# as in `@@family@@blurb` of 'vglm' object.
+# 
+# \item{cratio} {(no confidence ribbon)
+# conditional probability of 
+# scoring higher than a specific category, 
+# `logit(P[Y>j|Y>=j])` (without `logit`, 
+# which is accomplished by axis transformation in \link[ggplot2]{ggplot}) 
+# as in `@@family@@blurb` of 'vglm' object.}
+link_flatRate <- function(x, type = c('cumulative', 'acat', 'cratio'), ...) {
+  
+  # look at the use of `@family@linkinv` in ?VGAM::fittedvlm
+  fom <- attr(x, which = 'formula', exact = TRUE)
+  rnm <- deparse1(fom[[2L]])
+  rc <- x[[rnm]]
+  rlevels <- colnames(rc)
+  n <- length(rlevels)
+  prob <- attr(x, which = 'yhat', exact = TRUE) %||% rc / rowSums(rc)
+  
+  
+  ret <- switch(match.arg(type), cumulative = {
+    cumulative <- rowCumsums(prob)[, -n, drop = FALSE] # matrix
+    # cumulative is indeed plogis(VGAM::cumulative()@linkfun(prob))
+    colnames(cumulative) <- paste0(rlevels[-n], '-')
+    cumulative
+    
+  }, acat = {
+    acat <- prob[, -1L, drop = FALSE] / prob[, -n, drop = FALSE] # matrix
+    # acat is indeed exp(VGAM::acat()@linkfun(prob))
+    colnames(acat) <- paste0(rlevels[-1L], '/', rlevels[-n])
+    acat
+    
+  }, cratio = {
+    revcum <- rCumsums_rev(prob) # matrix
+    cratio <- revcum[, -1L, drop = FALSE] / revcum[, -n, drop = FALSE]
+    # VGAM::cratio()@linkfun(prob) # gives error?
+    colnames(cratio) <- paste0(rlevels[-1L], '+|', rlevels[-n], '+')
+    cratio
+    
+  })
+  
+  if (!length(ret) || !is.matrix(ret)) stop('rewrite code so that ret is always matrix')
+  return(ret)
+
+}
+
+
+
+rCumsums_rev <- function(x) { # slow anyway..
+  nc <- dim(x)[2L]
+  if (nc < 2L) return(x)
+  id <- nc:1
+  rowCumsums(x, cols = id)[, id, drop = FALSE]
+}
+
